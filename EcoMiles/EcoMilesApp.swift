09c8a1c020f7b7577390292ac883836967ed462c
Avani Shanbhag <shanbhagavani@gmail.com>
2025-07-15 11:04:16 -0400

    Initial Commit

---
+++ EcoMiles/EcoMilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoMilesApp.swift
+//  EcoMiles
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct EcoMilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
