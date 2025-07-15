a8c67dc1e4ca92f98f5170e249b67076d9d4ca35
Avani Shanbhag <shanbhagavani@gmail.com>
2025-07-15 11:12:44 -0400

    created all view pages

---
+++ EcoMiles/Welcome.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome.swift
+//  EcoMiles
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome()
+}
